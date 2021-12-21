75e731c60ac441f50a176d7dd1143a0296050da3
Yuliya.Dykava <weltraummm@gmail.com>
2021-12-21 14:41:35 +0300

    all main app logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import moment from "moment";
+
+const URL = "https://ttlivewebapi.fxopen.net:8443/api/v2/public/level2/EURUSD";
+const DELAY = 1000;
 
 function App() {
+  const [time, setTime] = useState(0);
+  const [bid, setBid] = useState(0);
+  const [ask, setAsk] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      fetch(URL)
+        .then((response) => response.json())
+        .then((json) => {
+          setTime(json[0].Timestamp);
+          setBid(json[0].BestBid.Price);
+          setAsk(json[0].BestAsk.Price);
+        });
+    }, DELAY);
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <p className="title">THE BEST</p>
+      <p className="title">TRADE OFFERS</p>
+      <div className="currency">
+        <div>
+          <p className="small_text">bid</p>
+          <p className="green price">{bid}</p>
+        </div>
+        <div>
+          <p className="small_text">ask</p>
+          <p className="red price">{ask}</p>
+        </div>
+      </div>
+      <div>
+        <p className="small_text">for the</p>
+        <p className="date_text">{moment(time).toString()}</p>
+      </div>
     </div>
   );
 }
